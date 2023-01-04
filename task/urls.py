27bc3d2e2ab59bf188dc4b57b2962fe800ccbf8e
Jean <Jean.c.drawing@gmail.com>
2023-01-04 14:09:02 -0300

    update task implemented

--- task/urls.py
+++ task/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
+from project.urls import home
+
 from . import views
 
 app_name = 'task'
 
 urlpatterns = [
     path('create/', views.create_task, name="create_task"),
-    path('update/', views.update_task, name="update_task"),
+    path('', home, name="home"),
+    path('update/<int:id>', views.update_task, name="update_task"),
+    path('delete/<int:id>', views.delete_task, name="delete_task"),
 ]
--- task/views.py
+++ task/views.py
@@ -1,9 +1,13 @@
 
+from datetime import date
+
 from django.shortcuts import render
 
 from task.models import task
 
 # Create your views here.
+
+start_date = date.today()
 
 
 def create_task(request):
@@ -15,16 +19,40 @@ def create_task(request):
         date_form = request.POST.get('date')
         add = task.objects.create(title=title, date=date_form)
         add.save()
-        return render(request, 'task/pages/create.html')
+        return render(request, 'task/pages/create.html', context={
+            'date': start_date
+        })
 
 
-def update_task(request):
-    return render(request, 'task/pages/update.html')
+def update_task(request, id):
+    task_info = task.objects.get(id=id)
+    if request.method == "GET":
+        return render(request, 'task/pages/update.html', context={
+            'task': task_info,
+            'date': start_date,
+        })
+
+    # else:
 
 
 def home(request):
-    task_ = task.objects.all().order_by('-title')
+    task_ = task.objects.all().order_by('-id')
     context = {
         'task': task_
     }
     return render(request, 'task/pages/home.html', context=context)
+
+
+def delete_task(request, id):
+    if request.method == 'GET':
+        return render(request, 'task/pages/delete.html')
+
+    else:
+        task_ = task.objects.all().order_by('-id')
+        context = {
+            'task': task_
+        }
+        deleting = task.objects.get(pk=id)
+
+        deleting.delete()
+        return render(request, 'task/pages/home.html', context=context)
